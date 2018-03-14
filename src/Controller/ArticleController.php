52febace809c285e52a98ced5bfb1aa07e78d72b
webDev <tess.hsu@gmail.com>
2018-03-14 09:32:38 +0100

    First commit

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]);
+    }
+}
