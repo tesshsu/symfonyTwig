37000a2055b2c73f55e3f12c4166749601a9e48e
webDev <tess.hsu@gmail.com>
2018-03-27 16:23:08 +0200

    catch service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -31,7 +31,7 @@ class ArticleController extends AbstractController
         ];
 
         $articleContent = <<<EOF
-Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank **in dolore**. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
 **turkey** shank eu pork belly meatball non cupim.
