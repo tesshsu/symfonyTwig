0aa3926681a02dfe84b1fd697ae3006103b79ed5
webDev <tess.hsu@gmail.com>
2018-04-04 10:52:35 +0200

    Add service markdown chanelle

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -2,20 +2,27 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    private $logger;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $markdownLogger;
     }
 
     public function parse(string $source): string
     {
+        if (stripos($source, 'Spicy') !== false) {
+            $this->logger->info('They are talking about spicy again!');
+        }
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
